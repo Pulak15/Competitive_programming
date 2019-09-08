8488c227fc0f310075002657297340b76582113c
Pulak15 <40883078+Pulak15@users.noreply.github.com>
2019-09-08 18:33:20 +0530

    Add files via upload

---
+++ 101262/a.cpp
@@ -0,0 +1,21 @@
+	#include<bits/stdc++.h>
+	#define F first
+	#define S second
+        using ll = long long;
+        const ll mod = 1000000007;
+        const ll infi = 1000000000000000;
+        const ll md = 998244353;
+        using namespace std;
+        using vl = vector<ll>;
+        using pll =  pair<ll,ll>;
+        #define PI 3.14159265
+       
+     
+        int main() {
+            ios_base::sync_with_stdio(false);
+            cin.tie(NULL);
+            int n;
+            cin>>n;
+            cout<<(n)*(n-1);
+            return 0;
+        } 
